1bae3a124c6dc54c5cbbfb80d0c29a0587807743
kyle4628 <kyle4628@gmail.com>
2020-11-19 10:16:12 +0800

    Modify duration time of showing error message

--- src/utils/request.js
+++ src/utils/request.js
@@ -77,7 +77,7 @@ service.interceptors.response.use(
     Message({
       message: error.message,
       type: 'error',
-      duration: 100 * 1000
+      duration: 10 * 1000
     })
     return Promise.reject(error)
   }
